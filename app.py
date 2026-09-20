889a4305f817cc72d1f8212fa92438d216a427c8
IsacAndrew <252948357+IsacAndrew@users.noreply.github.com>
2026-09-20 18:53:12 -0300

    Layout finalizado calculadora

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-# from flask import *
+from flask import *
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
-# def anexar_html():
-#     return render_template("index.html")
-
-input=("Digite o primeiro número:")
+@app.route("/")
+def anexar_html():
+    return render_template("index.html")
 
 
 
 
 
 
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
