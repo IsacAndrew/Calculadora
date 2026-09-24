8e0ab5418a09549999b3807f5d03fa302fb69926
IsacAndrew <252948357+IsacAndrew@users.noreply.github.com>
2026-09-24 09:13:26 -0300

    Versão com comentários para estudo

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.secret_key = "calculadora_isac"
 
 def rota():
     ultima_interacao = 0
+    resultado = None
 
     if request.method == "POST":
 
@@ -23,7 +24,7 @@ def rota():
         operacao_variavel = request.form.get("operacao_html")
         numero_variavel = request.form.get("numero_html")
 
-        #Defini o primeiro numero da session e o segundo numero da session
+        #Define o primeiro e segundo número da session
         if numero_variavel is not None:
             if "primeiro_numero_session" not in session:
                 session["primeiro_numero_session"] = numero_variavel
@@ -32,7 +33,7 @@ def rota():
                 session["segundo_numero_session"] = numero_variavel
                 ultima_interacao = session["segundo_numero_session"]
         
-        #Atribui a session á variavel do python
+        #Atribui a variavél do python a sua respectiva session
         if operacao_variavel is not None:
             session["operacao_session"] = operacao_variavel
             ultima_interacao = session["operacao_session"]
@@ -42,23 +43,16 @@ def rota():
             ultima_interacao = session["acao_session"]
 
         #Atribui a variavel ao valor da session naquela session, na session 
-        #sequinte o valor muda
+        # seguinte o valor muda
         primeiro_numero = session.get("primeiro_numero_session")
         segundo_numero = session.get("segundo_numero_session")
         operacao_variavel = session.get("operacao_session")
-
-        #Temporário enquanto o visor não funciona,
-        #só p ver se os valores estão indo certo
-        print(session.get("primeiro_numero_session"))
-        print(session.get("operacao_session"))
-        print(session.get("segundo_numero_session"))
-        print(f"Ultima interação = {ultima_interacao}")
 
         #Tratamento para limpar o ultimo digito do usuário, seja 1, +, 7, -, 4...
         if ultima_interacao == "limpar":
             ultima_interacao = 0
 
-        #Transforma o primeiro e segundo numero em inteiro. Calculadora não funciona com número não inteiro 
+        #Transforma o primeiro e segundo numero em inteiro. Calculadora AINDA não funciona com número não inteiro 
         if acao_variavel == "resultado":
             if primeiro_numero is not None and segundo_numero is not None:
                 primeiro_numero = int(primeiro_numero)
@@ -74,49 +68,13 @@ def rota():
                 resultado = ((primeiro_numero) * (segundo_numero))
             ultima_interacao = resultado
 
+        print(f"Primeiro dig.  :{primeiro_numero}")
+        print(f"Operação sel.  :{operacao_variavel}")
+        print(f"Segundo dig.   :{segundo_numero}")
+        print(f"Resultado Fin. :{resultado}")#Ta dando erro em resultado, porque? Eu coloquei a variavel...
+
+
     return render_template("index.html", visor = ultima_interacao)
-
-    
-
-# print("-=-"*10, "Calculadora", "-=-"*10)
-
-# num_01 = int(input("Digite o seu primeiro número: "))
-# num_02 = int(input("Digite o seu segundo número: "))
-
-# print("-=-"*20)
-
-# while True:
-
-#      operacao = input("1 - Adição,\n"
-#                       "2 - Subtração,\n"
-#                       "3 - Multiplicação\n"
-#                       "Selecione a operação: ")
-
-#      if operacao != "1" and operacao != "2" and operacao != "3":
-#          print("\033c", end="")  
-#          print(f"Primeiro número selecionado: {num_01}\n"
-#                f"Segundo número selecionado:  {num_02}\n") 
-#          print("Selecione apenas as opções exibidas: 1, 2 ou 3")
-#          continue
-#      else:
-#         break
-
-# print("-=-"*20)
-
-# if operacao == "1":
-
-#     resultado = (num_01 + num_02)
-#     print(f"A soma de {num_01} + {num_02} é igual a {resultado}")
-
-# elif operacao == "2":
-
-#     resultado = (num_01 - num_02)
-#     print(f"A subtração de {num_01} - {num_02} é igual a {resultado}")
-
-# else:
-
-#     resultado = (num_01 * num_02)
-#     print(f"A multiplicação de {num_01} * {num_02} é igual a {resultado}")
 
 if __name__=="__main__":
     app.run()
